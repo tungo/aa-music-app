878654d87acbb82c20ede6f5bb9acea36c65c4ab
Tu Ngo <tungo8810@gmail.com>
2017-04-20 15:22:51 -0700

    Add routes for band album track

--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,15 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :show, :create]
   resource :session, only: [:new, :create, :destroy]
+
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, except: [:new, :index] do
+    resources :tracks, only: [:new]
+  end
+
+  resources :tracks, except: [:new, :index]
+
 end
