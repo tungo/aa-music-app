d9d7a28a1b7fdc3b560402dd9ac134fb61631229
Tu Ngo <tungo8810@gmail.com>
2017-04-20 12:06:15 -0700

    Add UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(user)
+      redirect_to users_url(@user)
+    else
+      flash.now[:errors] = "Can not create new user"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
