50d79daac88e1e1bb01d5a9e3f2d00538a8b43e6
Tu Ngo <tungo8810@gmail.com>
2017-04-20 13:34:28 -0700

    Add SessionsController with methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
 class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(session_params)
+    if @user
+      login!(@user)
+
+      @user.reset_session_token!
+      session[:session_token] = @user.session_token
+
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = "Can not login"
+      render :new
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    redirect_to session_url
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
 end
